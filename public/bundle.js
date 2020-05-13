4c88ed9cfe2256937d91c7b01fec9cb2e002a699
raicherodrigo <rodrigoraiche@outlook.com>
2020-05-13 20:55:57 -0300

    update challenge reactJS

---
+++ public/bundle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactDom = _interopRequireDefault(require("react-dom"));
+
+//import App from "./App";
+_reactDom.default.render( /*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(App, null)), document.getElementById("root"));
--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      //console.log(response);
+      setRepositories(response.data);
+    });
+  }, [] );
+
+
   async function handleAddRepository() {
     // TODO
+
+    const response = await api.post('repositories',{
+      title: `Desafio com JS ${Date.now()}`,
+      url: `https://github.com/raicherodrigo/desafio-conceitos-node/ ${Date.now()}`,
+      techs: ["Node.js", "JavaScript"]
+    });
+
+    console.log(setRepositories);
+    const repositorie = response.data;
+
+    setRepositories([...repositories, repositorie]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+     await api.delete(`repositories/${id}`);
+    //console.log(id);
+
+    setRepositories(repositories.filter(repositorie => repositorie.id !== id));
+    
   }
 
+  
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+         {repositories.map(repositorie => <li key={repositorie.id}>{repositorie.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repositorie.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
